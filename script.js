e34f055249db3efa9747461715b3df4110d29a82
alohasally <peacheeks@gmail.com>
2022-09-02 12:00:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.open-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function(){
     modal.classList.remove('hidden');
@@ -18,5 +18,11 @@ const closeModal = function(){
 for(let i = 0; i< btnsOpenModal.length; i++)
     btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click',closeModal);
-overlay.addEventListener('click',closeModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();    
+    } 
+});
